75eeac586910faad0c017a4081352fcb43a27ab2
Ilya Golubev <ilya.golubev@ingka.com>
2021-05-27 20:20:22 +0200

    Adds super feature

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 
 const pjson = require('./package.json');
 const express = require('express');
-const app = express();
+const webApplication = express();
 
 const HOST = '0.0.0.0';
 const port = 8080;
 
-app.get('/', (req, res) => {
+var getHandler = function(request, response) {
   const idValue = parseInt(Math.random() * 1000000, 10);
   console.log(`${pjson.version} and ${idValue}`);
-  res.send(`${pjson.version} and ${idValue}`)
-});
+  response.send(`${pjson.version} and ${idValue}`)
+}
 
-const server = app.listen(port, async () => {
+webApplication.get('/hello', getHandler);
+
+const server = webApplication.listen(port, async () => {
   try {
-    console.log(`Example app v ${pjson.version} listening at http://${HOST}:${port}`);
+    console.log(`Eduard Example app v ${pjson.version} listening at http://${HOST}:${port}`);
   } catch {
     server.close();
   }
